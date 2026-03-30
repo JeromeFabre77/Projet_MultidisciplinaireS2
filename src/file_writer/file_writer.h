836a8101096614161c2a8e7253f6068b9ce8faa0
Melvin Warnet <mwarnet@etudiant.univ-lr.fr>
2026-03-30 16:42:29 +0200

    FIX : change for the request for change

--- src/file_writer/file_writer.h
+++ src/file_writer/file_writer.h
@@ -3,50 +3,8 @@
 
 #include "../individual/individual.h"
 
-/**
- * @brief Save the datas of the individual and cities in a JSON file located to the file path returned. 
- * According to the structure Individual and City, the data is written with the following format :
- *
- *{
- *  "cities": [
- *    {
- *      "insee_code": "59350",
- *      "name": "Lille",
- *      "region_code": "32",
- *      "region_name": "Hauts-de-France",
- *      "dept_code": "59",
- *      "dept_name": "Nord",
- *      "postal_code": "59000",
- *      "population": 236710,
- *      "latitude": 50.62920000,
- *      "longitude": 3.05730000
- *    },
- *    ...
- *  ],
- *  "individual":v{
- *      "chru_count": 50000,
- *      "isolated_population": 32510,
- *      "score": 278856,
- *      "hospitals": [
- *        {
- *          "chru_count": 1,
- *          "covered_population": 263831,
- *          "bed_count": 1500,
- *          "location": {
- *             "latitude": 50.62920000,
- *             "longitude": 3.05730000
- *            }
- *        },
- *        ...
- *      ]
- *  }
- *}
- *
- * @param filepath A pointer to the char array where the filepath string will be stored. Size has to be at least 100.
- * @param individual The pointer to the individual to save.
- * @param cities The pointer to the array of cities to save.
- * @param cities_size The size of the array cities.
- */
+#define RESULT_FOLDER "../assets/results/"
+
 void save_individual_and_cities_in_JSON(char* filepath, Individual* individual, City* cities, int cities_size);
 
 
--- src/file_writer/json_file_writer.c
+++ src/file_writer/json_file_writer.c
@@ -5,8 +5,6 @@
 
 #include "file_writer.h"
 
-#define RESULT_FOLDER "../assets/results/"
-
 /**
  * @brief Opens a file stream for writing a file.
  * 
@@ -21,7 +19,7 @@ FILE* open_writing_stream(const char* filename) {
     FILE *f = fopen(filename, "w");
     
     if (f == NULL) {
-        printf("Error : Writing stream could not be opened.\n");
+        fprintf(stderr, "Error : Writing stream could not be opened.\n");
     }
     
     return f;
@@ -142,7 +140,50 @@ void json_end_array(FILE* file, int has_next) {
 
 
 
-
+/**
+ * @brief Save the datas of the individual and cities in a JSON file located to the file path returned. 
+ * According to the structure Individual and City, the data is written with the following format :
+ *
+ *{
+ *  "cities": [
+ *    {
+ *      "insee_code": "59350",
+ *      "name": "Lille",
+ *      "region_code": "32",
+ *      "region_name": "Hauts-de-France",
+ *      "dept_code": "59",
+ *      "dept_name": "Nord",
+ *      "postal_code": "59000",
+ *      "population": 236710,
+ *      "latitude": 50.62920000,
+ *      "longitude": 3.05730000
+ *    },
+ *    ...
+ *  ],
+ *  "individual":{
+ *      "chru_count": 50000,
+ *      "isolated_population": 32510,
+ *      "score": 278856,
+ *      "hospitals": [
+ *        {
+ *          "chru_count": 1,
+ *          "covered_population": 263831,
+ *          "bed_count": 1500,
+ *          "location": {
+ *             "latitude": 50.62920000,
+ *             "longitude": 3.05730000
+ *            }
+ *        },
+ *        ...
+ *      ]
+ *  }
+ *}
+ *
+ * @param filepath A pointer to the char array where the filepath string will be stored. Size has to be at least 100.
+ * @param individual The pointer to the individual to save.
+ * @param cities The pointer to the array of cities to save.
+ * @param cities_size The size of the array cities.
+ */
 void save_individual_and_cities_in_JSON(char* filepath, Individual* individual, City* cities, int cities_size) {
     get_filepath(filepath);
 
